02b3af835972f8d53730ecd9580eda126fa71d2b
paranthamank <paranthamanks1999@gmail.com>
2021-10-24 22:21:55 +0530

    dicee completed

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,40 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int ld = 1;
+  int rd = 1;
+  onBPressed() {
+    setState(() {
+      ld = Random().nextInt(6) + 1;
+      rd = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () => onBPressed(),
+              child: Image.asset('images/dice$ld.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () => onBPressed(),
+              child: Image.asset('images/dice$rd.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
